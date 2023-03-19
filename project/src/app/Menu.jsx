0af502e4b862fc6ec7fd8a8ab251c5fdf4b42b9f
indexpro24@gmail.com <indexpro24@gmail.com>
2023-03-19 18:52:34 +0300

    c

---
+++ project/src/app/Menu.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Menu() {
+    return (
+        <>
+            <Link href="/test">TEST</Link>
+            <Link href="/test2">TEST-2</Link>
+        </>
+    );
+}
--- project/src/app/layout.js
+++ project/src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Menu from "./Menu";
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body>{children}</body>
+      <body>
+        <Menu />
+        {children}
+      </body>
     </html>
   );
 }
