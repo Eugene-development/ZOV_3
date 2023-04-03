bb02afc840f4036f45eb6ec2e889d323315f4c36
indexpro24@gmail.com <indexpro24@gmail.com>
2023-04-03 07:54:31 +0300

    c

--- project/src/app/module/components/Header/Menu/index.jsx
+++ project/src/app/module/components/Header/Menu/index.jsx
@@ -84,10 +84,10 @@ export default function Menu() {
 
       <nav className="mx-auto flex max-w-full items-center justify-between p-3 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
