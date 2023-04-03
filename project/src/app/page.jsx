e2c62b1a5e6b53cb986472250db7f1a768c12263
indexpro24@gmail.com <indexpro24@gmail.com>
2023-04-03 18:28:13 +0300

    c

--- project/src/app/page.jsx
+++ project/src/app/page.jsx
@@ -2,16 +2,16 @@ import Hero from "./z-module/components/Hero";
 import Brand from "./z-module/components/Brand";
 import Testimonials from "./z-module/components/Testimonials";
 import ExampleFurniture from "./z-module/components/ExampleFurniture";
+import Feature from "./z-module/components/Feature";
+
 export default async function Home() {
   return (
     <>
-    <Hero/>
-    <Brand/>
-    <ExampleFurniture/>
-    <Testimonials/>
-      <h1 className="font-serif text-3xl font-bold underline text-red-800 pl-8">
-        Home
-      </h1>
+      <Hero/>
+      <Brand/>
+      <ExampleFurniture/>
+      <Feature/>
+      <Testimonials/>
     </>
   );
 }
