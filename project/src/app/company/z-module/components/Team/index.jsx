5f11f4e41e643d95215f267bbb2524f79a1eed1e
Eugene Che <indexpro24@gmail.com>
2023-04-21 20:50:15 +0300

    c

--- project/src/app/company/z-module/components/Team/index.jsx
+++ project/src/app/company/z-module/components/Team/index.jsx
@@ -1,18 +1,18 @@
 const team = [
   {
-    name: 'Michael Foster',
+    name: 'Michael Fosterasasasas',
     role: 'Co-Founder / CTO',
     imageUrl:
       'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
   },
   {
-    name: 'Michael Foster',
+    name: 'Michael Fosteredcercerc',
     role: 'Co-Founder / CTO',
     imageUrl:
       'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
   },
   {
-    name: 'Michael Foster',
+    name: 'Michael Foster233',
     role: 'Co-Founder / CTO',
     imageUrl:
       'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
