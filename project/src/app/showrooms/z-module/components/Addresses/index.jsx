aa5756c6ab400fd8c164619051f64c83ab886ea6
Eugene Che <indexpro24@gmail.com>
2023-04-25 20:20:48 +0300

    с

--- project/src/app/showrooms/z-module/components/Addresses/index.jsx
+++ project/src/app/showrooms/z-module/components/Addresses/index.jsx
@@ -1,5 +1,48 @@
-import Image from "next/image"
-import Link from "next/link"
+const showrooms = [
+    {
+        city: 'Москва',
+        address: 'Мичуринский проспект,  10к1',
+        metro: 'Ломоносовский проспект',
+        img: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'Мичуринский проспект,  10к1',
+        metro: 'Ломоносовский проспект',
+        img: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'Мичуринский проспект,  10к1',
+        metro: 'Ломоносовский проспект',
+        img: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'Мичуринский проспект,  10к1',
+        metro: 'Ломоносовский проспект',
+        img: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'Мичуринский проспект,  10к1',
+        metro: 'Ломоносовский проспект',
+        img: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'Мичуринский проспект,  10к1',
+        metro: 'Ломоносовский проспект',
+        img: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png',
+        link: '/'
+    },
+]
+
 export default function Addresses() {
   return (
     <aside aria-label="Related articles" className="bg-white dark:bg-gray-900 py-8 lg:py-20">
@@ -9,23 +52,25 @@ export default function Addresses() {
                 <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">В связи с высокой загруженностью дизайнеров просьба предупредить о Вашем визите.</p>
             </div> 
             <div className="grid gap-6 lg:gap-12 md:grid-cols-2">
-                <article className="flex flex-col xl:flex-row">
-                    <div className="mb-2 xl:mb-0">
-                        <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png" className="mr-5 max-w-sm" alt="Image 2"></img>
-                    </div>
-                    <div className="flex flex-col justify-center">
-                        <p className="mb-2 text-2xl font-bold leading-tight text-gray-900 dark:text-white">
-                            <span>Москва</span>
-                        </p>
-                        <p className="mb-2 text-lg font-bold leading-tight text-gray-900 dark:text-white">
-                            <span>Мичуринский проспект,  10к1</span>
-                        </p>
-                        <p className="mb-4 font-light text-gray-500 dark:text-gray-400 max-w-sm">Станция метро "Ломоносовский проспект"</p>
-                        <div className="inline-flex items-center font-medium underline underline-offset-4 text-red-900 dark:text-primary-500 hover:no-underline">
-                            Записаться к дизайнеру
+                {showrooms.map(item => (
+                     <p className="flex flex-col xl:flex-row">
+                        <div className="mb-2 xl:mb-0">
+                            <img src={item.img} className="mr-5 max-w-sm" alt="Image 2"></img>
                         </div>
-                    </div>
-                </article>
+                        <div className="flex flex-col justify-center">
+                            <p className="mb-2 text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+                                <span>{item.city}</span>
+                            </p>
+                            <p className="mb-2 text-lg font-bold leading-tight text-gray-900 dark:text-white">
+                                <span>{item.address}</span>
+                            </p>
+                            <p className="mb-4 font-light text-gray-500 dark:text-gray-400 max-w-sm">Станция метро: <br/> "{item.metro}"</p>
+                            <div className="inline-flex items-center font-medium underline underline-offset-4 text-red-900 dark:text-primary-500 hover:no-underline">
+                                Записаться к дизайнеру
+                            </div>
+                        </div>
+                     </p>
+                ))} 
             </div>
         </div>
     </aside>
