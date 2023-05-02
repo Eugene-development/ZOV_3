7c3e5c629840a2501389c62107e002af83c76752
Eugene Che <indexpro24@gmail.com>
2023-05-02 11:51:25 +0300

    c

--- project/src/app/z-module/components/Header/index.jsx
+++ project/src/app/z-module/components/Header/index.jsx
@@ -1,11 +1,9 @@
 import Menu from "./Menu";
-import Catalog from "./Catalog";
 
 export default function Header() {
     return (
         <>
             <Menu/>
-            {/* <Catalog/> */}
         </>
     );
 }
