cd0c10e83bc5289cbc333938344932bdf1b7694e
Eugene Che <indexpro24@gmail.com>
2023-05-02 15:02:25 +0300

    c

--- project/src/app/z-module/components/Testimonials/index.jsx
+++ project/src/app/z-module/components/Testimonials/index.jsx
@@ -81,6 +81,7 @@ function QuoteIcon(props) {
 
 export default function Testimonials() {
   return ( 
+    <>
     <section
       id="testimonials"
       aria-label="What our customers are saying"
@@ -88,7 +89,7 @@ export default function Testimonials() {
     >
       <Container>
         <div className="mx-auto max-w-2xl md:text-center">
-          <h2 className="font-display text-3xl tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className=" text-3xl tracking-tight text-gray-900 sm:text-4xl">
             Отзывы о нашей работе.
           </h2>
           <p className="mt-4 text-lg tracking-tight text-gray-700">
@@ -120,9 +121,9 @@ export default function Testimonials() {
                             {testimonial.author.role}
                           </div>
                         </div>
-                        <div className="overflow-hidden rounded-full bg-gray-50">
+                        <div className="overflow-hidden rounded-sm bg-gray-50">
                           <Image
-                            className="h-14 w-14 object-cover"
+                            className="h-24 w-32 object-cover"
                             src={testimonial.author.image}
                             alt=""
                             width={56}
@@ -141,5 +142,33 @@ export default function Testimonials() {
       </Container>
 
     </section>
+
+    <div className="bg-white">
+      <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Boost your productivity.
+            <br />
+            Start using our app today.
+          </h2>
+          <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">
+            Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur
+            commodo do ea.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Get started
+            </a>
+            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              Learn more <span aria-hidden="true">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
