f6f0f7a0eb850f99afac4c653c02a7721ba29598
Eugene Che <indexpro24@gmail.com>
2023-05-10 15:52:03 +0300

    с

--- project/src/app/z-module/components/Feature/index.jsx
+++ project/src/app/z-module/components/Feature/index.jsx
@@ -48,8 +48,7 @@ export default function Feature() {
           <h2 className="text-base font-semibold leading-7 text-indigo-200">Это важно</h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Качество материалов</p>
           <p className="mt-6 text-lg leading-8 text-gray-300">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste
-            dolor cupiditate blanditiis.
+            Мы гордимся высочайшим качеством материалов, которые мы используем при производстве кухонных гарнитуров и шкафов.
           </p>
         </div>
         <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16">
