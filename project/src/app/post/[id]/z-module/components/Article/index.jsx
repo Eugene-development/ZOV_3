5bd14e25bbe52e6370b003045a7eb6419e6328b8
Eugene Che <indexpro24@gmail.com>
2023-05-14 15:59:01 +0300

    с

--- project/src/app/post/[id]/z-module/components/Article/index.jsx
+++ project/src/app/post/[id]/z-module/components/Article/index.jsx
@@ -41,9 +41,9 @@ export default function Article({ param }) {
                     </aside>
                 </div>
 
-<div className="my-8 text-base font-light text-gray-500 dark:text-gray-400">
+<div className="my-8 text-gray-500 dark:text-gray-400 prose font-normal">
     <p >Ваша кухня - сердце вашего дома. Это место, где вы готовите пищу, проводите время с семьей и друзьями, и создаете незабываемые моменты. Когда дело доходит до выбора кухонного гарнитура, один из самых важных аспектов, которые нужно учесть, это цвет фасадов, каркаса и столешницы. Цветовая схема вашей кухни может создать атмосферу и визуальный образ, отражающий вашу уникальность и стиль. В этой статье мы рассмотрим несколько ключевых аспектов, которые помогут вам сделать правильный выбор цветов для вашего кухонного гарнитура.</p>
-<ul>
+<ol>
     <li>
         Учтите размеры и освещение помещения:
     </li>
@@ -51,7 +51,7 @@ export default function Article({ param }) {
     <li>Учитывайте свой стиль:</li>
     <p>Цвет гарнитура должен соответствовать вашему собственному стилю и предпочтениям. Если вы предпочитаете классический стиль, то теплые и нейтральные оттенки, такие как бежевый или коричневый, будут отличным выбором. Если вы склонны к современным трендам, вы можете рассмотреть более смелые и яркие цвета, такие как глубокий синий или смарагдово-зеленый.</p>
     <li>Рассмотрите общий дизайн интерьера:</li>
-    <p>Важно принять во внимание общий дизайн интерьера вашего дома. Если у вас уже есть определенная цветовая палитра или дизайнерский стиль, вы можете выбрать цвета гарнитура, которые гармонично впишутся в общую концепцию. Например, если ваш дом о</p>
+    <p>Важно принять во внимание общий дизайн интерьера вашего дома. Если у вас уже есть определенная цветовая палитра или дизайнерский стиль, вы можете выбрать цвета гарнитура, которые гармонично впишутся в общую концепцию. Например, если ваш интерьер в ярких цветах, то тональность самой мебели имеет смысл выбрать в спокойном пастельном варианте.</p>
     <li>Рассмотрите практичность и функциональность:</li>
     <p>Помимо эстетических аспектов, важно учесть практичность и функциональность выбранных цветов. Некоторые оттенки могут быть более подвержены пятнам и загрязнениям, особенно на фасадах кухонного гарнитура. В этом случае, выберите цвета, которые легко чистятся и не требуют особенного ухода. Кроме того, стоит учитывать, что темные оттенки могут создавать визуальный эффект уменьшения размеров помещения, поэтому в маленьких кухнях они могут быть не самым лучшим выбором.</p>
     <li>Сделайте акценты:</li>
@@ -60,7 +60,7 @@ export default function Article({ param }) {
     <p>Не бойтесь проявить свою творческую сторону и выбрать необычные цветовые решения для вашего кухонного гарнитура. В конце концов, ваша кухня - это ваше пространство для самовыражения. Рассмотрите использование разных оттенков одного цвета или смешение разных цветов для создания уникального и стильного образа. Вы можете играть с контрастами или использовать неожиданные комбинации цветов, чтобы придать вашей кухне особый характер и индивидуальность.</p>
     <p>В завершение, выбор цветов для фасадов, каркаса и столешницы вашего кухонного гарнитура - это важное решение, которое оказывает влияние на общий внешний вид и атмосферу вашей кухни. При выборе цветов учтите размеры и освещение помещения, свой собственный стиль, общий дизайн интерьера, практичность и функциональность, акценты и будущие тенденции. Не бойтесь быть творческими и экспериментировать с различными цветовыми сочетаниями.</p>
     <p>Помните, что выбранные цвета должны создавать гармонию, комфорт и отражать вашу индивидуальность. И не забывайте, что цветовые решения можно всегда обновить и изменить с течением времени. Главное - слушайте свой вкус и создавайте кухню, которая будет радовать вас и вашу семью на протяжении многих лет.</p>
-</ul>
+</ol>
 </div>
 
 
@@ -489,57 +489,57 @@ export default function Article({ param }) {
         </main>
 
         <aside aria-label="Related articles" className="py-8 lg:py-24 bg-gray-50 dark:bg-gray-800">
-        <div className="px-4 mx-auto max-w-screen-xl">
-            <h2 className="mb-8 text-2xl font-bold text-gray-900 dark:text-white">Читать далее</h2>
-            <article className="flex mb-8">
-                <a href="#" className="shrink-0">
-                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png" className="mr-5 w-32 h-32 max-w-fullalign-middle rounded-full" alt="Image 1"/>
-                </a>
-                <div className="flex flex-col justify-center">
-                    <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
-                        <a href="#">Our first office</a>
-                    </h2>
-                    <p className="mb-2 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation.</p>
-                    <a href="#" className="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
-                        Read in 2 minutes
+            <div className="px-4 mx-auto max-w-screen-xl">
+                <h2 className="mb-8 text-2xl font-bold text-gray-900 dark:text-white">Читать далее</h2>
+                <article className="flex mb-8">
+                    <a href="#" className="shrink-0">
+                        <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png" className="mr-5 w-32 h-32 max-w-fullalign-middle rounded-full" alt="Image 1"/>
                     </a>
-                </div>
-            </article>
-            <article className="flex mb-8">
-                <a href="#" className="shrink-0">
-                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-2.png" className="mr-5 w-32 h-32 max-w-full align-middle rounded-full" alt="Image 2"/>
-                </a>
-                <div className="flex flex-col justify-center">
-                    <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
-                        <a href="#">Enterprise design tips</a>
-                    </h2>
-                    <p className="mb-2 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation.</p>
-                    <a href="#" className="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
-                        Read in 12 minutes
+                    <div className="flex flex-col justify-center">
+                        <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                            <a href="#">Our first office</a>
+                        </h2>
+                        <p className="mb-2 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation.</p>
+                        <a href="#" className="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                            Read in 2 minutes
+                        </a>
+                    </div>
+                </article>
+                <article className="flex mb-8">
+                    <a href="#" className="shrink-0">
+                        <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-2.png" className="mr-5 w-32 h-32 max-w-full align-middle rounded-full" alt="Image 2"/>
                     </a>
-                </div>
-            </article>
-            <article className="flex">
-                <a href="#" className="shrink-0">
-                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-3.png" className="mr-5 w-32 h-32 max-w-full align-middle rounded-full" alt="Image 3"/>
-                </a>
-                <div className="flex flex-col justify-center">
-                    <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
-                        <a href="#">We partnered up with Google</a>
-                    </h2>
-                    <p className="mb-2 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation.</p>
-                    <a href="#" className="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
-                        Read in 8 minutes
+                    <div className="flex flex-col justify-center">
+                        <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                            <a href="#">Enterprise design tips</a>
+                        </h2>
+                        <p className="mb-2 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation.</p>
+                        <a href="#" className="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                            Read in 12 minutes
+                        </a>
+                    </div>
+                </article>
+                <article className="flex">
+                    <a href="#" className="shrink-0">
+                        <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-3.png" className="mr-5 w-32 h-32 max-w-full align-middle rounded-full" alt="Image 3"/>
                     </a>
-                </div>
-            </article>
-        </div>
+                    <div className="flex flex-col justify-center">
+                        <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                            <a href="#">We partnered up with Google</a>
+                        </h2>
+                        <p className="mb-2 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation.</p>
+                        <a href="#" className="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                            Read in 8 minutes
+                        </a>
+                    </div>
+                </article>
+            </div>
         </aside>
 
         <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div className="mx-auto max-w-screen-md sm:text-center">
-                <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Sign up for our newsletter</h2>
+                <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Подписаться на наши новости</h2>
                 <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">Stay up to date with the roadmap progress, announcements and exclusive discounts feel free to sign up with your email.</p>
                 <form action="#">
                     <div className="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
@@ -548,13 +548,13 @@ export default function Article({ param }) {
                             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                 <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                             </div>
-                            <input className="block p-3 pl-10 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your email" type="email" id="email" required=""/>
+                            <input className="block p-3 pl-10 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Введите вашу почту" type="email" id="email" required=""/>
                         </div>
                         <div>
-                            <button type="submit" className="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Subscribe</button>
+                            <button type="submit" className="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Подписаться</button>
                         </div> 
                     </div>
-                    <div className="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">We care about the protection of your data. <a href="#" className="font-medium text-primary-600 dark:text-primary-500 hover:underline">Read our Privacy Policy</a>.</div>
+                    <div className="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">Мы заботимся о защите ваших данных. <a href="#" className="font-medium text-primary-600 dark:text-primary-500 hover:underline">Политика безопасности</a>.</div>
                 </form>
             </div>
         </div>
