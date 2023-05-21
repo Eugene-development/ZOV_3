5e47b6d8df8c6db22bb2910f702321e6277b7c3a
Eugene Che <indexpro24@gmail.com>
2023-05-21 08:42:15 +0300

    c

--- project/src/app/catalog/kuhna/page.jsx
+++ project/src/app/catalog/kuhna/page.jsx
@@ -3,7 +3,7 @@ import Filter from "./z-module/components/Filter/index"
 export default function Kuhna() {
   return (
     <>
-        <Filter/>
+        {/* <Filter/> */}
         <section class="bg-white dark:bg-gray-900 antialiased">
           <div class="max-w-screen-xl px-4 py-8 mx-auto sm:py-10 ">
 
@@ -28,11 +28,6 @@ export default function Kuhna() {
             <div class="max-w-3xl p-4 mx-auto mt-8 rounded-md lg:mt-16 bg-gray-50 dark:bg-gray-800">
               <div class="flex flex-col justify-between gap-3 md:gap-6 md:items-center md:flex-row">
                 <div class="flex items-center gap-1.5">
-                  <svg aria-hidden="true" class="hidden w-5 h-5 text-gray-800 dark:text-gray-400 md:block shrink-0"
-                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path
-                      d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
-                  </svg>
                   <span class="text-base font-normal text-gray-700 dark:text-gray-400">
                     <span class="font-semibold">
                       Вы можете вызвать замерщика. Это бесплатно.
