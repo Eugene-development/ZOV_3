1d15302afda047ebda580e95234d4dac4f56cf2f
Eugene Che <indexpro24@gmail.com>
2023-05-26 20:13:09 +0300

    с

--- project/src/app/z-module/components/Hero/Slider/index.jsx
+++ project/src/app/z-module/components/Hero/Slider/index.jsx
@@ -19,13 +19,13 @@ export default function Slider() {
         className="rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10 mt-8 sm:mt-12"
         />
         <img
-        src="https://zovofficial.com/image/cache/catalog/products/kuhni/2023/antiko/1-1600x1086.jpg"
-        alt="кухонный гарнитур"
+        src="https://zovrus.ru/files/_cache/product/garderob-3.1.resize800x800w.png?1c559b168e4119b9eb35ca543d2341fd"
+        alt="гардероб"
         className="rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10 mt-8 sm:mt-12"
         />
         <img
-        src="https://zovofficial.com/image/cache/catalog/products/kuhni/2023/antiko/1-1600x1086.jpg"
-        alt="кухонный гарнитур"
+        src="https://zovrus.ru/files/_cache/product/podvesnaya-sistema-3.resize1200x1200w.png?59ad289b075212d895b4cd2c94665f8f"
+        alt="шкаф купе"
         className="rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10 mt-8 sm:mt-12"
         />
     </Carousel >
